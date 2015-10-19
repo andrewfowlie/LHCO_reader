fa9b9b2c0a30b050cc41828d5e5c31a926f60118
andrew-laptop <andrew.fowlie@kbfi.ee>
2015-10-19 12:50:40 +1100

    fixed mocks

--- docs/conf.py
+++ docs/conf.py
@@ -286,3 +286,14 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
