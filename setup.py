54d9f20fc14f36f20c6c569909e895797670caff
andrew-laptop <andrew.fowlie@kbfi.ee>
2015-10-22 12:33:19 +1100

    Release 1.1, arXiv

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'LHCO_reader',
   packages = ['LHCO_reader'], # this must be the same as the name above
-  version = '1.0.2',
+  version = '1.1,
   description = 'Module for reading and analyzing LHCO files',
   author = 'Andrew Fowlie',
   author_email = 'Andrew.Fowlie@Monash.Edu.Au',
   url = 'https://github.com/innisfree/LHCO_reader',
-  download_url = 'https://github.com/innisfree/LHCO_reader/tarball/1.0.2',
+  download_url = 'https://github.com/innisfree/LHCO_reader/tarball/1.1',
   keywords = ['LHCO', 'HEP', 'ROOT'],
   classifiers = [],
 )
