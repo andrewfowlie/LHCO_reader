909fd3892d1baaf6ac5343a697aa42e89a6f2845
andrew-laptop <andrew.fowlie@kbfi.ee>
2015-11-02 09:59:45 +1100

    Minor typo.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'LHCO_reader',
   packages = ['LHCO_reader'], # this must be the same as the name above
-  version = '1.42'
+  version = '1.42',
   description = 'Module for reading and analyzing LHCO files',
   author = 'Andrew Fowlie',
   author_email = 'Andrew.Fowlie@Monash.Edu.Au',
