a4204808c87f3680bd1992f65cd8024d686c8a8b
andrew-laptop <andrew.fowlie@kbfi.ee>
2016-01-04 15:15:32 +1100

    Adding example events again!

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(name='LHCO_reader',
       packages=['LHCO_reader'],  # this must be the same as the name above
-      version='1.6.1',
+      version='1.6.2',
       install_requires=['prettytable', 'numpy'],
       package_data={'example': ['example.lhco']},
       description='Module for reading and analyzing LHCO files',
       author='Andrew Fowlie',
       author_email='Andrew.Fowlie@Monash.Edu.Au',
       url='https://github.com/innisfree/LHCO_reader',
-      download_url='https://github.com/innisfree/LHCO_reader/tarball/1.6.0',
+      download_url='https://github.com/innisfree/LHCO_reader/tarball/1.6.2',
       keywords=['LHCO', 'HEP', 'ROOT'],
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Programming Language :: Python :: 2.7',
